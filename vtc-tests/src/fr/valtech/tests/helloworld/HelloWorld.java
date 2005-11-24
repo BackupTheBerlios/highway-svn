17d491ef1f5af1441b110ad885ca4f799e67fc64
jbaumgarten <jbaumgarten@2522ca6a-2706-0410-af84-ca202e22bfdb>
2005-11-24 00:27:26 +0000

    Initial import of vtc-tests. Mainly to test integration between Eclipse and BerliOS SVN.

---
+++ vtc-tests/src/fr/valtech/tests/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package fr.valtech.tests.helloworld;
+
+public class HelloWorld {
+	final static private String HELLO_WORLD = "Hello World";
+	public static void main(String[] args) {
+		System.out.println(HELLO_WORLD);
+	}
+
+}
