703e895fb69fb66e102571976992caf7073f71e2
nlaine <nlaine@2522ca6a-2706-0410-af84-ca202e22bfdb>
2006-07-11 16:32:35 +0000

    repackaging tests classes

---
+++ highway/trunk/test_core/src/org/highway/database/hibernate/onetableperclasshierarchy/ChequePaymentDef.java
@@ -0,0 +1,13 @@
+package org.highway.database.hibernate.onetableperclasshierarchy;
+
+import org.highway.database.DiscriminatorValue;
+import org.highway.database.Mapped;
+import org.highway.database.MappedOn;
+
+@Mapped
+@DiscriminatorValue("CHEQUE")
+public interface ChequePaymentDef extends PaymentDef
+{
+	@MappedOn("ORDERNAME")
+	String getOrder();
+}
---
+++ highway/trunk/test_core/src/org/highway/database/hibernate/onetableperclasshierarchy/PaymentDef.java
@@ -0,0 +1,20 @@
+package org.highway.database.hibernate.onetableperclasshierarchy;
+
+import org.highway.bean.ValueObject;
+import org.highway.database.DiscriminatorColumn;
+import org.highway.database.Identity;
+import org.highway.database.Mapped;
+import org.highway.database.MappedOn;
+
+@Mapped
+@MappedOn("PAYMENT1")
+@DiscriminatorColumn(column="PAYMENT_TYPE", type="java.lang.String")
+public interface PaymentDef extends ValueObject
+{
+	@Identity
+	@MappedOn("ID")
+	long getPaiementId();
+	
+	@MappedOn("AMOUNT")
+	Integer getAmount();
+}
